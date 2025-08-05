7253f428b84a66db7ad4ac35d3282116c8e24ca8
Lucas <zhanglucas128@gmail.com>
2025-08-05 03:17:04 +0000

    Added more functions to the app

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ TODOLIST = ToDoList()
 #
 
 
+
+
 app = Flask(__name__)
+
 
 ## Instantiate your database here:
 #
@@ -20,20 +23,26 @@ def home():
     # Complete the code below
     # The todo_list variable should be returned by running a scan on your DDB table,
     # which is then converted to a list
-    tasks = TODOLIST.get_tasks()
+    search_term = request.args.get("search_term")
+    if search_term is None:
+        search_term = ""
+    tasks = TODOLIST.get_tasks(search_term=search_term)
     task_number = len(tasks)
     is_blank = request.args.get('is_blank')
     is_duplicate = request.args.get('is_duplicate')
 
+
     # can leave this line as is to use the template that's provided
-    return render_template("index.html", tasks=tasks, task_number=task_number, is_blank=is_blank, is_duplicate=is_duplicate)
+    return render_template("index.html", tasks=tasks, task_number=task_number, is_blank=is_blank, is_duplicate=is_duplicate, search_term=search_term)
+
+
 
 
 @app.route("/add", methods=["POST"])
 def add():
     title = request.form.get("title")
     description = request.form.get("description")
-    is_successful = TODOLIST.add_task(title)
+    is_successful = TODOLIST.add_task(title, description)
     is_duplicate = False
     is_blank = False
     if is_successful == "Task name cannot be empty":
@@ -41,9 +50,12 @@ def add():
     elif is_successful == "Task is already added to the list":
         is_duplicate = True
 
+
     return redirect(url_for("home", is_duplicate=is_duplicate, is_blank=is_blank))
 
-    
+
+   
+
 
 @app.route("/update/<title>")
 def update(title):
@@ -53,14 +65,24 @@ def update(title):
     return redirect(url_for("home"))
 
 
+
+
 @app.route("/delete/<title>")
 def delete(title):
     # Complete the code below to delete an item from the to-do list
     TODOLIST.delete_task(title)
     return redirect(url_for("home"))
 
-# app.py
-import os
+
+@app.route("/search", methods=["POST"])
+def search():
+    search_term = request.form.get("search")
+    return redirect(url_for("home", search_term=search_term))
+
+
+
+
+
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, port=3000)
--- src/todo_list.py
+++ src/todo_list.py
@@ -6,6 +6,7 @@ from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute, BooleanAttribute
 from datetime import datetime
 
+
 class Task(Model):
     class Meta:
         table_name = "todo-table"
@@ -13,12 +14,14 @@ class Task(Model):
     title = UnicodeAttribute(hash_key=True)
     date_created = UnicodeAttribute()
     status = BooleanAttribute()
+    description = UnicodeAttribute()
+
 
 class ToDoList:
     def __init__(self):
         if not Task.exists():
             Task.create_table(read_capacity_units=1, write_capacity_units=1, wait = True)
-        
+       
     def validate_task_name(self, name : str):
         """
         Validate the task name. Task names cannot be empty or whitespace only.
@@ -26,8 +29,9 @@ class ToDoList:
         if name == "" or name.isspace():
             return "Task name cannot be empty"
 
+
         return True
-    
+   
     def validate_task_not_exists(self, name : str):
         """
         Validates if the task is already added.
@@ -37,24 +41,27 @@ class ToDoList:
             if name == task["title"]:
                 return "Task is already added to the list"
 
-        return True 
 
-        
-    def add_task(self, title):
+        return True
+
+
+       
+    def add_task(self, title, description):
         """
         This method should add a task to the DynamoDB table
         """
         if self.validate_task_name(title) == "Task name cannot be empty":
-            print("1")
             return "Task name cannot be empty"
-        
+       
         if self.validate_task_not_exists(title) == "Task is already added to the list":
-            print("2")
             return "Task is already added to the list"
 
-        task = Task(title=title, status=False, date_created=str(datetime.now().date()))
+
+        task = Task(title=title, status=False, date_created=str(datetime.now().date()), description=description)
         task.save()
         return True
+
+
 
 
     def get_tasks(self, search_term=""):
@@ -66,10 +73,11 @@ class ToDoList:
         all_tasks = [task.attribute_values for task in Task.scan()]
         if search_term == "":
             return all_tasks
-        
+       
         tasks = [task for task in all_tasks if search_term in task["title"]]
         return tasks
-        
+       
+
 
     def update_task(self, title):
         """
@@ -80,6 +88,7 @@ class ToDoList:
         task.save()
         return True
 
+
     def delete_task(self, title):
         """
         This method should delete the task with the given todo_id from the DynamoDB table.
@@ -87,4 +96,4 @@ class ToDoList:
         task = Task.get(hash_key=title)
         task.delete()
         return True
-    
+   
